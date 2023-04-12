a04f4533d69640426ed9e63c244f7c932fcac42e
marcelo <marcelogilosmoreira@gmail.com>
2023-04-12 12:01:43 -0300

    Desafio banco concluido, porem pode melhorar

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -7,14 +7,42 @@ public class Desafio2 {
         double saldo = 0;
         int sair = 0;
         String menu;
+        String nome;
+        String conta;
+
+        System.out.print("""
+                ************************************
+                Bem vindo ao banco JAVA!
+                Iremos fazer seu cadastro
+                ************************************
+                Nome:""");
+        nome = scanner.nextLine();
+
+        System.out.print("Qual o tipo de conta (Corrente ou Poupança?):");
+
+        conta = scanner.nextLine();
+
         while(sair == 0){
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            System.out.println("Escolha 1 numero do menu ai");
+            System.out.println(String.format("""
+                **************************************
+                Dados Iniciais Cliente
+
+                Nome: %s
+                Tipo de conta: %s
+                Saldo: %.2f
+                **************************************
+                1-Veja seu Saldo
+                2-Receba um valor
+                3-Retire um valor
+                4-Sair
+            """,nome,conta,saldo));
             menu = scanner.nextLine();
             switch(menu){
                 case "1":
                 System.out.println(String.format("O seu saldo atual é de : R$ %.2f", saldo));
                 scanner.nextLine();
+                System.out.println("Pressione Enter para continuar.");
                 break;
                 case "2":
                 System.out.println("Quanto você transferiu para sua conta?");
@@ -22,6 +50,7 @@ public class Desafio2 {
                 valorEntrada.replaceAll(",", ".");
                 saldo += Double.parseDouble(valorEntrada);
                 System.out.println("***Valor adicionado com sucesso***");
+                System.out.println("Pressione Enter para continuar.");
                 scanner.nextLine();
                 break;
                 case "3":
@@ -31,6 +60,7 @@ public class Desafio2 {
                 saldo -= Double.parseDouble(valorSaida);
                 System.out.println("***Valor transferido com sucesso***");
                 scanner.nextLine();
+                System.out.println("Pressione Enter para continuar.");
                 break;
                 case "4":
                 System.out.println("**Obrigado por usar o Banco Meu primeiro JAVA***");
@@ -39,9 +69,11 @@ public class Desafio2 {
                 default:
                 System.out.println("Valor passado invalido, por favor escolha uma das opções validas.");
                 scanner.nextLine();
+                System.out.println("Pressione Enter para continuar.");
                 break;
             }
         };
+        scanner.close();
         
     }
 }
